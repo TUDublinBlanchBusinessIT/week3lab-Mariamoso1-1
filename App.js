9121c326e7c0d233bb55f3e0ab311b16da9bf3d6
B00162501 Mariam Osomoku <B00162501@mytudublin.ie>
2025-09-29 16:17:51 +0100

    first changes

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
 import {View, Text, TextInput} from 'react-native';
 import React, {useState} from 'react';
+import Logo from './components/logo'
 
-
-export default function App(){
-  const [fullname, setFullname] = useState("Eoin");
-  return(
-    <View> 
-      <Text>Hello, World {fullname}</Text>
-      <TextInput placeholder="enter your name" onChangeText={(value)=>setFullname(value)}></TextInput>
+export default function App() {
+const {fullname, setFullname} = useState("Mary");
+const [fname, setFname] = useState("Mariam");
+const [lname, setLname] = useState("Osomoku");
+const [dob, setDob] = useState("05 January 2004");
+  return (
+    <View >
+    <Logo/><Text>Hello {fname} {lname}. You were born on {dob}</Text>
+      <TextInput stylplaceholder="enter your name" onChangeText={setFullname}/>
+      <TextInput placeholder="Enter your firstname" onChangeText={setFname}/>
+      <TextInput placeholder="Enter your lastname" onChangeText={setLname}/>
+      <TextInput placeholder="Enter your date of birth" onChangeText={setDob}/>
     </View>
   )
 };
+
 
 
 
